4d71c03cfaa405b01e760af4ce3608ab29e5dce9
AleksandarP91 <Pejcinovski91@gmail.com>
2015-10-19 13:28:03 +0200

    Project Uppload

---
+++ MvcDemo/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult Membership()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult Membership(MvcDemo.Models.Member formData)
+        {
+            return View("Thanks", formData);
+        }
+    }
+}
