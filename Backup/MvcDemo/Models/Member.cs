10e9ebee3e80e8d1c032d7927807e52c7fd019ea
AleksandarP91 <Pejcinovski91@gmail.com>
2015-10-19 13:35:30 +0200

    Project Uppload

---
+++ Backup/MvcDemo/Models/Member.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcDemo.Models
+{
+    public class Member : Controller
+    {
+        //
+        // GET: /Member/
+
+        public int MemberID{ get; set; }
+        public string MemberName{ get; set; }
+        public string MemberAddress{ get; set; }
+        public int MemberAge{ get; set; }
+
+    }
+}
